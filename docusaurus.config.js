d7b19380a22cc07dc4bcaceef999510ff710ce1d
Mehmet Arif Emre Şen <maemresen@yazilim.vip>
2020-11-05 18:46:19 +0300

    design: top-menu  item removed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,39 @@
 const [latestVersion] = require("./versions.json");
 
+console.log("latestVersion", latestVersion);
+
+let topMenuItems = [
+  {
+    to: "docs/",
+    activeBasePath: "docs",
+    label: "Docs",
+    position: "right",
+  },
+  {
+    to: "versions",
+    label: `Releases`,
+    position: "right",
+  },
+  {
+    href: "https://github.com/yazilim-vip/spring-vip/blob/master/LICENSE",
+    className: "header-license-link",
+    position: "right",
+  },
+
+  {
+    href: "https://github.com/yazilim-vip/spring-vip",
+    className: "header-stars-link",
+    position: "right",
+  },
+];
+
+if (latestVersion !== undefined && latestVersion !== null) {
+  topMenuItems.push({
+    type: "docsVersionDropdown",
+    position: "left",
+  });
+}
+
 module.exports = {
   title: "Spring VIP",
   tagline:
@@ -17,39 +51,11 @@ module.exports = {
         alt: "Spring VIP Logo",
         src: "img/yvip_logo_design_vip_only.png",
       },
-      items: [
-        {
-          type: "docsVersionDropdown",
-          position: "left",
-        },
-        {
-          to: "docs/",
-          activeBasePath: "docs",
-          label: "Docs",
-          position: "right",
-        },
-        {
-          to: "versions",
-          label: `Releases`,
-          position: "right",
-        },
-        {
-          href: "https://github.com/yazilim-vip/spring-vip/blob/master/LICENSE",
-          className: "header-license-link",
-          position: "right",
-        },
-
-        {
-          href: "https://github.com/yazilim-vip/spring-vip",
-          className: "header-stars-link",
-          position: "right",
-        },
-      ],
+      items: topMenuItems,
     },
     footer: {
       style: "dark",
-      links: [ 
-      ],
+      links: [],
       copyright: `Copyright © ${new Date().getFullYear()} Yazilim VIP, Inc. Built with Docusaurus.`,
     },
   },
@@ -62,8 +68,7 @@ module.exports = {
           homePageId: "gettingStartedQuickStart",
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl:
-            "https://github.com/yazilim-vip/spring-vip-wiki/blob/master",
+          editUrl: "https://github.com/yazilim-vip/spring-vip-wiki/blob/master",
         },
         blog: {
           showReadingTime: true,
